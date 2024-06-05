470e9d1524490c8ee9d4ffef17c40995d0f133ae
Madushan <Madushan@DESKTOP-O2MVSJ2>
2024-06-05 09:52:40 +0530

    postgres

--- database/sequelize.ts
+++ database/sequelize.ts
@@ -1,13 +1,33 @@
+// import { Sequelize } from 'sequelize';
+
+// const sequelize = new Sequelize({
+//     dialect: 'mysql',
+//     dialectModule: require('mysql2'),
+//     host: '3.111.92.159',
+//     port: 3306,
+//     database: 'yovibez_chatbot',
+//     username: 'yovibez_chatbot',
+//     password: 'IFXx63a2jDyNdf73lf23bfk@ff'
+//   });
+
+// export default sequelize;
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize({
-    dialect: 'mysql',
-    dialectModule: require('mysql2'),
-    host: '3.111.92.159',
-    port: 3306,
-    database: 'yovibez_chatbot',
-    username: 'yovibez_chatbot',
-    password: 'IFXx63a2jDyNdf73lf23bfk@ff'
-  });
+    dialect: 'postgres',
+    dialectModule: require('pg'),
+    host: 'ep-patient-dew-a4qwbn7j-pooler.us-east-1.aws.neon.tech', 
+    port: 5432,
+    database: 'verceldb', 
+    username: 'default', 
+    password: 'iLRzX32vyrDA', 
+    ssl: true, 
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false 
+        }
+    }
+});
 
 export default sequelize;
