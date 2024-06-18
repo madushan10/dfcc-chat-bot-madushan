6f0552122b4ed177b3c0eef637d39964bda41e4d
Madushan <Madushan@DESKTOP-O2MVSJ2>
2024-06-18 16:20:51 +0530

    twilio 3

--- database/sequelize.ts
+++ database/sequelize.ts
@@ -1,35 +1,14 @@
-// import { Sequelize } from 'sequelize';
-
-// const sequelize = new Sequelize({
-//     dialect: 'mysql',
-//     dialectModule: require('mysql2'),
-//     host: '3.111.92.159',
-//     port: 3306,
-//     database: 'yovibez_chatbot',
-//     username: 'yovibez_chatbot',
-//     password: 'IFXx63a2jDyNdf73lf23bfk@ff'
-//   });
-
-// export default sequelize;
 import { Sequelize } from 'sequelize';
-import pg from 'pg';
 
 const sequelize = new Sequelize({
-    dialect: 'postgres',
-    dialectModule: pg,
-    host: 'ep-patient-dew-a4qwbn7j-pooler.us-east-1.aws.neon.tech', 
-    port: 5432,
-    database: 'verceldb', 
-    username: 'default', 
-    password: 'iLRzX32vyrDA', 
-    ssl: true, 
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false 
-        }
-    }
-});
+    dialect: 'mysql',
+    dialectModule: require('mysql2'),
+    host: '3.111.92.159',
+    port: 3306,
+    database: 'yovibez_chatbot',
+    username: 'yovibez_chatbot',
+    password: 'IFXx63a2jDyNdf73lf23bfk@ff'
+  });
 
 export default sequelize;
 
